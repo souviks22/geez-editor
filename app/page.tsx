5134d59fc3df50db1c49b410de1bcaebbbf83a02
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-07-31 20:16:39 +0530

    Custom interface for text editing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default () => {
+export default function Home() {
   return (<main className='flex min-h-screen flex-col items-center justify-between p-24'>
     Geez
   </main>)
---
+++ types/slate.d.ts
@@ -0,0 +1,78 @@
+import { BaseEditor, BaseElement } from "slate"
+import { ReactEditor } from "slate-react"
+import { HistoryEditor } from "slate-history"
+
+export type CustomText = {
+  text: string,
+  bold?: boolean,
+  italic?: boolean,
+  underline?: boolean,
+  code?: boolean,
+  color?: string,
+  backgroundColor?: string,
+  fontSize?: number,
+  fontStyle?: string,
+  script?: 'sub' | 'super',
+}
+
+export type CommonElement = {
+  align?: 'left' | 'center' | 'right' | 'justify',
+  spacing?: number,
+  backgroudColor?: string,
+  borderWidth?: number,
+  borderColor?: string
+} & BaseElement
+
+export type ParagraphElement = { type: 'paragraph' } & CommonElement
+
+export type QuoteBlockElement = { type: 'quote-block' } & CommonElement
+
+export type BulletedListElement = { type: 'bulleted-list' } & CommonElement
+
+export type NumberedListElement = { type: 'numbered-list' } & CommonElement
+
+export type ListItemElement = { type: 'list-item' } & CommonElement
+
+export type HeadingElement = { type: 'heading', level: number } & CommonElement
+
+export type ImageElement = { type: 'image', url: string } & CommonElement
+
+export type LinkElement = { type: 'link', url: string } & CommonElement
+
+export type TableElement = { type: 'table' } & CommonElement
+
+export type TableRowElement = { type: 'table-row' } & CommonElement
+
+export type TableHeaderElement = { type: 'table-header' } & CommonElement
+
+export type TableCellElement = { type: 'table-cell' } & CommonElement
+
+export type CodeBlockElement = { type: 'code-block', language: string } & CommonElement
+
+export type CodeLineElement = { type: 'code-line' } & CommonElement
+
+export type CustomElement =
+  | ParagraphElement
+  | QuoteBlockElement
+  | BulletedListElement
+  | NumberedListElement
+  | ListItemElement
+  | HeadingElement
+  | ImageElement
+  | LinkElement
+  | TableElement
+  | TableRowElement
+  | TableHeaderElement
+  | TableCellElement
+  | CodeBlockElement
+  | CodeLineElement
+
+export type CustomEditor = BaseEditor & ReactEditor & HistoryEditor
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: CustomEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
