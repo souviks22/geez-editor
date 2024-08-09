caf275101a7696c08484222da37e11104a37865a
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-09 23:50:53 +0530

    Sign In button enhanced

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -6,19 +6,17 @@ import Protect from "../auth/Protect"
 export default function Navbar() {
   const { data: session } = useSession()
   const [auth, setAuth] = useState<boolean>(false)
-  return (<div className={`flex justify-end fixed z-10 w-full ${!auth && 'backdrop-blur'}`}>
-    <section className='text-lg transition-colors p-5'>
-      {session
-        ?
-        <p onClick={() => signOut()} className='px-5 py-1 cursor-pointer hover:bg-sky-100 animate-pulse hover:animate-none rounded'>
-          Sign Out
-        </p>
-        :
-        <p onClick={() => setAuth(true)} className='px-5 py-1 cursor-pointer hover:bg-sky-100 animate-pulse hover:animate-none rounded'>
-          Sign In
-        </p>
-      }
-    </section>
+  return (<div className={`flex justify-end fixed z-10 w-full px-28 py-5 ${!auth && 'backdrop-blur'}`}>
+    {session
+      ?
+      <p onClick={() => signOut()} className='bg-sky-50 hover:bg-sky-100 px-5 py-1 m-5 cursor-pointer transition-colors duration-200 rounded shadow'>
+        Sign Out
+      </p>
+      :
+      <p onClick={() => setAuth(true)} className='bg-sky-50 hover:bg-sky-100 px-5 py-1 m-5 cursor-pointer transition-colors duration-200 rounded shadow'>
+        Sign In
+      </p>
+    }
     {auth && <Protect onClick={() => setAuth(false)} />}
   </div>)
 }
