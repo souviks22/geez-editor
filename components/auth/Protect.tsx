397bf8f9eb9158dab346753343ffd52b0e0da326
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-12 22:52:49 +0530

    Account section added in navbar

--- components/auth/Protect.tsx
+++ components/auth/Protect.tsx
@@ -7,7 +7,7 @@ export default function Protect({ except, onClick = () => { } }: Readonly<{ exce
   return (<>
     {!except && !session &&
       <div
-        className='w-full h-full absolute z-10 flex min-h-screen flex-col items-center justify-between p-24 backdrop-blur'
+        className='w-full h-full absolute right-0 z-10 flex min-h-screen flex-col items-center justify-between p-24 backdrop-blur'
         onClick={onClick}
       >
         <Authentication />
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,17 +1,32 @@
 "use client"
 import { useState } from "react"
 import { useSession, signOut } from "next-auth/react"
+import Image from "next/image"
 import Protect from "../auth/Protect"
 
 export default function Navbar() {
   const { data: session } = useSession()
   const [auth, setAuth] = useState<boolean>(false)
-  return (<div className={`flex justify-end fixed z-10 w-full px-28 py-5 ${!auth && 'backdrop-blur'}`}>
+  const [menu, setMenu] = useState<boolean>(false)
+  return (<div className={`flex justify-end fixed z-10 w-full px-28 py-2 ${!auth && 'backdrop-blur'}`}>
     {session
       ?
-      <p onClick={() => signOut()} className='bg-sky-50 hover:bg-sky-100 px-5 py-1 m-5 cursor-pointer transition-colors duration-200 rounded shadow'>
-        Sign Out
-      </p>
+      <section className='relative cursor-pointer' onClick={() => setMenu(menu => !menu)}>
+        <Image
+          src={session.user?.image || '/user.png'}
+          alt='User'
+          width={40}
+          height={40}
+          className='rounded-full'
+        />
+        {menu &&
+          <div className='w-32 bg-white absolute rounded my-3 tran'>
+            <p onClick={() => signOut()} className='bg-sky-50 hover:bg-sky-100 px-5 py-1 cursor-pointer transition-colors duration-200 rounded shadow'>
+              Sign Out
+            </p>
+          </div>
+        }
+      </section>
       :
       <p onClick={() => setAuth(true)} className='bg-sky-50 hover:bg-sky-100 px-5 py-1 m-5 cursor-pointer transition-colors duration-200 rounded shadow'>
         Sign In
